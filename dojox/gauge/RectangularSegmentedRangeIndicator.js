c8b7d56c92733139de2025464ae5226bdb59dfc1
Christophe Jolif <cjolif@gmail.com>
2011-10-20 17:17:16 +0200

    try to better follow dojo styleguide

--- dojox/gauge/RectangularSegmentedRangeIndicator.js
+++ dojox/gauge/RectangularSegmentedRangeIndicator.js
@@ -57,20 +57,19 @@ define(["dojo/_base/declare", "dojo/on", "dojox/gfx", "./IndicatorBase"], functi
 				
 				var rangeStart = 0;
 				
-				for (var i = 0; i < this.ranges.length; i++) {
+				for(var i = 0; i < this.ranges.length; i++){
 					var entry1 = {
 						color:this.ranges[i].color,
 						offset: scale.scaler.positionForValue(rangeStart)
-					}
+					};
 					var entry2 = {
 						color:this.ranges[i].color,
 						offset: scale.scaler.positionForValue(rangeStart+this.ranges[i].size)
-					}
+					};
 					fill.colors.push(entry1);
 					fill.colors.push(entry2);
 					rangeStart += this.ranges[i].size;
 				}
-				
 			}else if (fill && fill.colors){
 				// Configure gradient
 				fill.x1 = startX;
@@ -90,33 +89,32 @@ define(["dojo/_base/declare", "dojo/on", "dojox/gfx", "./IndicatorBase"], functi
 			var remain = visibleSegments - Math.floor(visibleSegments);
 			
 			
-            for (i = 0; i < Math.floor(visibleSegments); i++) {		
-				var path=group.createPath();
+            for(i = 0; i < Math.floor(visibleSegments); i++){
+				var path = group.createPath();
                 
-                if (i == 0 && this.rounded && (sw/2) < chicklet) { // first segment rounded
+                if(i == 0 && this.rounded && (sw/2) < chicklet){ // first segment rounded
 					radius = sw/2;
                     path.moveTo(x + radius, y);
                     path.lineTo(x + chicklet, y);
                     path.lineTo(x + chicklet, y + ew);
                     path.lineTo(x + radius, y + sw);
                     path.arcTo(radius, radius, 0, 0, 1, x + radius, y)
-                }
-                else 
-                    if (i == Math.floor(visibleSegments) - 1 && (remain == 0) && this.rounded && (ew/2) < chicklet) { // last segment rounded
+                }else{
+                    if(i == Math.floor(visibleSegments) - 1 && (remain == 0) && this.rounded && (ew/2) < chicklet){ // last segment rounded
 						radius = ew/2;
                         path.moveTo(x, y);
                         path.lineTo(x + chicklet - radius, y);
                         path.arcTo(radius, radius, 0, 0, 1, x + chicklet - radius, y + ew)
                         path.lineTo(x, y + sw);
                         path.lineTo(x, y);
-                    }
-                    else {
+                    }else{
                         path.moveTo(x, y);
                         path.lineTo(x + chicklet, y);
                         path.lineTo(x + chicklet, y + ew);
                         path.lineTo(x, y + sw);
                         path.lineTo(x, y);
-                    }	
+                    }
+				}
 				
 				path.setFill(fill).setStroke(stroke);
 				sw = ew;
@@ -125,7 +123,7 @@ define(["dojo/_base/declare", "dojo/on", "dojox/gfx", "./IndicatorBase"], functi
             }
 			
 			// draw the remaining segment part
-			if( remain > 0 ){
+			if(remain > 0){
 				ew = sw+( (ew-sw)*remain );
 				var gp = [x, y, x+(chicklet*remain), y, x+(chicklet*remain), y  + ew, x, y + sw, x, y]
 				shape = group.createPolyline(gp).setFill(fill).setStroke(stroke);
@@ -146,20 +144,19 @@ define(["dojo/_base/declare", "dojo/on", "dojox/gfx", "./IndicatorBase"], functi
 				
 				var rangeStart = 0;
 				
-				for (var i = 0; i < this.ranges.length; i++) {
+				for(var i = 0; i < this.ranges.length; i++){
 					var entry1 = {
 						color:this.ranges[i].color,
 						offset: scale.scaler.positionForValue(rangeStart)
-					}
+					};
 					var entry2 = {
 						color:this.ranges[i].color,
 						offset: scale.scaler.positionForValue(rangeStart+this.ranges[i].size)
-					}
+					};
 					fill.colors.push(entry1);
 					fill.colors.push(entry2);
 					rangeStart += this.ranges[i].size
 				}
-				
 			}else if (fill && fill.colors){
 				// Configure gradient
 				fill.x1 = startX;
@@ -178,34 +175,33 @@ define(["dojo/_base/declare", "dojo/on", "dojox/gfx", "./IndicatorBase"], functi
 			var ew = sw+inc;			
 			var remain = visibleSegments - Math.floor(visibleSegments);
 			
-            for (i = 0; i < Math.floor(visibleSegments); i++) {		
-				var path=group.createPath();
+            for(i = 0; i < Math.floor(visibleSegments); i++){
+				var path = group.createPath();
                 
-                if (i == 0 && this.rounded && (sw/2) < chicklet) { // first segment rounded
+                if(i == 0 && this.rounded && (sw/2) < chicklet){ // first segment rounded
 					radius = sw/2;
                     path.moveTo(x , y+ radius);
                     path.lineTo(x , y+ chicklet);
                     path.lineTo(x + ew, y + chicklet);
                     path.lineTo(x + sw, y + radius);
                     path.arcTo(radius, radius, 0, 0, 0, x , y+ radius)
-                }
-                else 
-                    if (i == Math.floor(visibleSegments) - 1 && (remain == 0) && this.rounded && (ew/2) < chicklet) { // last segment rounded
+                }else{
+                    if(i == Math.floor(visibleSegments) - 1 && (remain == 0) && this.rounded && (ew/2) < chicklet){ // last segment rounded
 						radius = ew/2;
                         path.moveTo(x, y);
                         path.lineTo(x , y+ chicklet - radius);
                         path.arcTo(radius, radius, 0, 0, 0, x + ew, y + chicklet - radius)
                         path.lineTo(x+ sw, y );
                         path.lineTo(x, y);
-                    }
-                    else {
+                    }else{
                         path.moveTo(x, y);
                         path.lineTo(x , y+ chicklet);
                         path.lineTo(x + ew, y + chicklet);
                         path.lineTo(x+ sw, y );
                         path.lineTo(x, y);
                     }	
-				
+				}
+
 				path.setFill(fill).setStroke(stroke);
 				sw = ew;
 				ew += inc;
@@ -213,12 +209,11 @@ define(["dojo/_base/declare", "dojo/on", "dojox/gfx", "./IndicatorBase"], functi
             }
 			
 			// draw the remaining segment part
-			if( remain > 0 ){
+			if(remain > 0){
 				ew = sw+( (ew-sw)*remain );
 				var gp = [x, y, x, y+(chicklet*remain), x+ ew, y+(chicklet*remain), x+ sw, y , x, y]
 				shape = group.createPolyline(gp).setFill(fill).setStroke(stroke);
 			}					
-			
 			
 			return shape;
 		},
@@ -238,7 +233,7 @@ define(["dojo/_base/declare", "dojo/on", "dojox/gfx", "./IndicatorBase"], functi
 		
 		refreshRendering: function(){
 		
-			if (this._gfxGroup == null || this.scale == null) {
+			if(this._gfxGroup == null || this.scale == null){
 				return;
 			}
 			// gets position corresponding to the values
@@ -249,11 +244,11 @@ define(["dojo/_base/declare", "dojo/on", "dojox/gfx", "./IndicatorBase"], functi
 			var startX;
 			var startY;
 			var endPosition;
-			if (this.scale._gauge.orientation == "horizontal") {
+			if (this.scale._gauge.orientation == "horizontal"){
 				startX = spos;
 				startY = this.paddingTop;
 				endPosition = pos;
-			} else {
+			}else{
 				startX = this.paddingLeft;
 				startY = spos ;
 				endPosition = pos;			

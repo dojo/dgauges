b5c3210c1bc2bef10865bde0a6092fc328661234
Christophe Jolif <cjolif@gmail.com>
2011-10-20 15:38:34 +0200

    trailing commas fixes

--- dojox/gauge/IndicatorBase.js
+++ dojox/gauge/IndicatorBase.js
@@ -4,6 +4,6 @@ define(["dojo/_base/declare", "../widget/_Invalidating"], function(declare, _Inv
 	 var _Invalidating = dojox.widget._Invalidating;
 	 =====*/
 	return declare("dojox.gauge.IndicatorBase", _Invalidating, {
-		value: null,
+		value: null
 	});
 });
